17293e925eedbaf214458d5c7a2144d5ca61bf75
prasanth <shenoy.prasanth@gmail>
2018-02-26 08:58:46 -0600

    started implementation of moment js for date and month handling

--- js/eventContent.js
+++ js/eventContent.js
@@ -2,6 +2,7 @@
 
     $(function () {
         attachEvents();
+        DataContainer.setinfo();
     });
 
     var attachEvents = function () {
@@ -21,11 +22,37 @@
         //     $('.month1,.month2').fadeIn(1000, "swing");    
         // }, 2000); 
 
-        $('.month1,.month2').animate({ opacity: '0' }, 600, 'linear');
+        $('.month1,.month2').animate({ opacity: '0' }, 1000, 'linear');
 
         setTimeout(function () {
-            $('.month1,.month2').animate({ 'opacity': '1' }, 1400, 'linear');
-        }, 1000);
+            $('.month1,.month2').animate({ 'opacity': '1' }, 700, 'swing');
+        }, 300);
     };
 
 })(jQuery);
+
+var DataContainer = function () {
+    var months = moment.months();
+    var pastmonth = '', currentmonth = '', nextmonth = '', futuremonth = '';
+
+    console.log(moment().month(0).format('MMMM'));
+
+    console.log(moment().add(0, "month").format('MMMM'));
+
+    var setinfo = function (monthindex) {
+        index = monthindex || moment().month()
+        pastmonth = moment().add(index - 1, 'month').format('MMMM');
+        currentmonth = moment().format('MMMM');
+        nextmonth = moment().add( 1, 'month').format('MMMM');                
+        futuremonth = moment().add( 2, 'month').format('MMMM');
+        console.log(pastmonth + ' ' + currentmonth + ' ' + nextmonth + ' ' + futuremonth);                
+    }
+
+    return {
+        setinfo: setinfo,
+        pastmonth: pastmonth,
+        currentmonth: currentmonth,
+        nextmonth: nextmonth,
+        futuremonth: futuremonth
+    }
+}();
